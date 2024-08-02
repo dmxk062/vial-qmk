0bc6d58b918eb23ae351094edd9de5f3b0a304ea
dmxk062 <mxk06@protonmail.com>
2024-08-02 10:09:06 +0200

    vial: add own config for capslock

---
+++ keyboards/keychron/q12/ansi_encoder/keymaps/jhk/rules.mk
@@ -0,0 +1,9 @@
+VIA_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+RAW_ENABLE = yes
+VIAL_ENABLE = yes
+VIALRGB_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
+
+VPATH += keyboards/keychron/common
+SRC += keychron_common.c
